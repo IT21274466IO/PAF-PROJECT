8b62d8e0298a36b49de174646a5fe87196315035
IT21274466IO <it21274466@my.sliit.lk>
2024-05-11 20:49:43 +0530

    created update user modal

--- PAF_Frontend/src/components/userProfile/UserProfile.jsx
+++ PAF_Frontend/src/components/userProfile/UserProfile.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import "./userProfile.css";
 import { Link } from "react-router-dom";
 
@@ -11,6 +11,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faFeed, faLink, faMessage } from "@fortawesome/free-solid-svg-icons";
 
 export default function UserProfile() {
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal){
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Add logic to handle form submission and update user details
+    toggleModal(); // Close the modal after form submission
+  };
+  
   return (
     <div className="userProfile">
       <div className="cover-photos">
@@ -28,7 +47,7 @@ export default function UserProfile() {
               <FontAwesomeIcon icon={faMessage} />
             </button>
           </Link>
-          <button className="btn btn-primary">
+          <button className="btn btn-primary" onClick={toggleModal}>
             <FontAwesomeIcon icon={faEdit} /> Edit Profile
           </button>
           <button className="btn">
@@ -39,6 +58,34 @@ export default function UserProfile() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sapiente necessitatibus alias iure deleniti. Eos blanditiis fugit quos quam ad!
         </p>
       </div>
+      {modal && (
+        <div className="modal">
+          <div className="overlay" onClick={toggleModal}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+              <h2 className="editProfileModel">Edit Profile</h2>
+              <form onSubmit={handleFormSubmit}>
+                <label htmlFor="profilePicture">Profile Picture:</label>
+                <input type="file" id="profilePicture" name="profilePicture" placeholder="Enter new profile picture URL"/>
+                
+                <label htmlFor="coverPhoto">Cover Photo:</label>
+                <input type="file" id="coverPhoto" name="coverPhoto" placeholder="Enter new cover photo URL"/>
+                
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName" placeholder="Enter new first name"/>
+                
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" placeholder="Enter new last name"/>
+                
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Enter new email address"/>
+
+                <button type="submit" className="btn btn-primary">Save Changes</button>
+                <button type="button" className='btn btn-red' onClick={toggleModal}>Cancel</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
